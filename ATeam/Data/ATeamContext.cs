2774da2ac9d5cfce9e69ea03c2a2ec50c31d539f
ATeam <year3project2020@outlook.com>
2020-02-03 12:35:18 +0800

    init setup

---
+++ ATeam/Data/ATeamContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATeam.Models
+{
+    public class ATeamContext : DbContext
+    {
+        public ATeamContext (DbContextOptions<ATeamContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ATeam.Models.PatientInput> PatientInput { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<DoctorConsultation>()
+                .HasKey(co => new { co.ID, co.PatientID });
+
+            builder.Entity<DoctorConsultation>()
+                .HasOne(co => co.PatientInput)
+                .WithMany(c => c.DoctorConsultations)
+                .HasForeignKey(co => co.ID);
+
+            builder.Entity<DoctorConsultation>()
+                .HasOne(co => co.Consultation)
+                .WithMany(o => o.DoctorConsultations)
+                .HasForeignKey(co => co.PatientID);
+        }
+    }
+}
---
+++ ATeam/Models/DoctorConsultation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATeam.Models
+{
+    public class DoctorConsultation
+    {
+        public string ID { get; set; }
+        public PatientInput PatientInput { get; set; }
+
+        public string PatientID { get; set; }
+        public Consultation Consultation { get; set; }
+    }
+}
